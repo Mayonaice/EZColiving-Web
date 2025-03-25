6191371ca384fb9d18aa4761d6d919fcd4ce6fee
Your Name <vadodali131@gmail.com>
2025-03-26 04:29:22 +0700

    tes

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://ezcoliving-web.up.railway.app'),
 
     /*
     |--------------------------------------------------------------------------
