d00b20077c8e32b8c83a883bb8f7b046572e6052
Your Name <vadodali131@gmail.com>
2025-05-06 02:49:44 +0700

    add contact page

--- resources/views/user/layout.blade.php
+++ resources/views/user/layout.blade.php
@@ -85,25 +85,15 @@
                 <div
                     class="relative group xsm:h-10 xsm:w-10 xl:h-14 xl:w-14 flex items-center justify-center rounded-lg cursor-pointer hover:text-white hover:bg-green-700 hover:duration-300 hover:ease-linear focus:bg-gray-500 {{ request()->routeIs('user.about') ? 'bg-green-700 text-white' : 'text-gray-700' }}">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
-                    </svg>
-                    <span
-                        class="absolute xsm:left-12 xl:left-16 bg-green-700 text-white px-2 py-1 text-sm rounded-lg opacity-0 transform -translate-x-2 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300">Tentang Kami</span>
-                </div>
-            </a>
-
-            <div
-                class="relative group xsm:h-10 xsm:w-10 xl:h-14 xl:w-14 flex items-center justify-center rounded-lg cursor-pointer hover:text-white hover:bg-green-700 hover:duration-300 hover:ease-linear focus:bg-gray-500">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
                 </svg>
-                <span
-                    class="absolute xsm:left-12 xl:left-16 bg-green-700 text-white px-2 py-1 text-sm rounded-lg opacity-0 transform -translate-x-2 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300">Contact</span>
-            </div>
+                    <span
+                        class="absolute xsm:left-12 xl:left-16 bg-green-700 text-white px-2 py-1 text-sm rounded-lg opacity-0 transform -translate-x-2 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300">Contact</span>
+                </div>
+            </a>
+
         </aside>
 
         <div class="h-full flex-1 flex flex-col">
